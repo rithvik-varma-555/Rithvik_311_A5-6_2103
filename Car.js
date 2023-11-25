b541023cc804f0a2329c2cd927f86f822cbc1698
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-11-24 19:13:51 -0500

    construtor created

---
+++ Car.js
@@ -0,0 +1,9 @@
+class Car {
+    constructor(brand, model, year, color, price, gas) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.gas = gas;
+      }
