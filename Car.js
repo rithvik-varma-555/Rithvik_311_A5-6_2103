4149863f3416091fe382335ea396cdc0c03a050f
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-11-24 19:24:47 -0500

    invoking honk for each object

--- Car.js
+++ Car.js
@@ -42,3 +42,10 @@ const bmw = new Car("BMW", "X5", 2022, "Green", 60000, 65);
 const mazda = new Car("Mazda", "CX-5", 2019, "White", 15000, 60);
 const audi = new Car("Audi", "Q7", 2018, "Silver", 52000, 47);
 const kia = new Car("Kia", "Forte", 2020, "Blue", 21000, 56);
+
+honda.honk();
+ford.honk();
+bmw.honk();
+mazda.honk();
+audi.honk();
+kia.honk();
